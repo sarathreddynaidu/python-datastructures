d730a4197d64311a79290ecdb9a0638aea900410
Sarath Chandra Reddy Naidu <sarathreddynaidu@gmail.com>
2021-11-16 00:19:12 -0500

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,43 +1,139 @@
-class LLNode:
-    def __init__(self, value, nextNode=None):
-        self.value = value
-        self.nextNode = nextNode
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
 
-class LL:
-    def __init__(self, head=None):
-        self.head = head
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-    def insert_begin(self, value):
-        node = LLNode(value, self.head)
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + ' --> '
+            itr = itr.next
+        print(llstr)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
+
+        return count
+
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
         self.head = node
 
-    def insert_end(self, value):
-        node = LLNode(value)
+    def insert_at_end(self, data):
         if self.head is None:
-            self.head = node
+            self.head = Node(data, None)
             return
-        else:
-            currentNode = self.head
-            while True:
-                if currentNode.nextNode is None:
-                    currentNode.nextNode = node
-                    break
-                else:
-                    currentNode = currentNode.nextNode
 
-    def printLL(self):
-        currentNode = self.head
-        while currentNode is not None:
-            print(currentNode.value, "-> ", end="")
-            currentNode = currentNode.nextNode
-        print("None")
+        itr = self.head
 
-ll = LL()
-ll.printLL()
-ll.insert_begin(10)
-ll.printLL()
-ll.insert_begin(45)
-ll.printLL()
-ll.insert_end(55)
-ll.printLL()
+        while itr.next:
+            itr = itr.next
 
+        itr.next = Node(data, None)
+
+    def insert_at(self, index, data):
+        if index<0 or index>self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.insert_at_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+    def remove_at(self, index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count+=1
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+
+    def insert_after_value(self, data_after, data_to_insert):
+        if self.head is None:
+            return
+
+        if self.head.data==data_after:
+            self.head.next = Node(data_to_insert,self.head.next)
+            return
+
+        itr = self.head
+        while itr:
+            if itr.data == data_after:
+                itr.next = Node(data_to_insert, itr.next)
+                break
+
+            itr = itr.next
+
+    def remove_by_value(self, data):
+        if self.head is None:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_values(["banana","mango","grapes","orange"])
+    ll.print()
+    ll.insert_after_value("mango","apple")
+    ll.print()
+    ll.remove_by_value("orange")
+    ll.print()
+    ll.remove_by_value("figs")
+    ll.print()
+    ll.remove_by_value("banana")
+    ll.remove_by_value("mango")
+    ll.remove_by_value("apple")
+    ll.remove_by_value("grapes")
+    ll.print()
+
+    # ll.insert_values([45,7,12,567,99])
+    # ll.insert_at_end(67)
+    # ll.print()
